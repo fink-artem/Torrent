3ad0ae64b928bc8b1a98982c22f8bfaf06b88f6a
author <author@mail.ru>
2016-12-05 20:45:48 +0700

    Изменён тест

--- src/test/java/com/torrent/MainTest.java
+++ src/test/java/com/torrent/MainTest.java
@@ -1,16 +1,15 @@
-
 package com.torrent;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MainTest {
-    
+
     @Test
-    public void testMain() {
-        String[] args = null;
+    public void testMain() throws BadArgumentsException {
+        String[] args = {"1", "127.0.0.1:20201"};
         Main.main(args);
         assertTrue(true);
     }
-    
+
 }
