2877410cdab647a9c77a4bc542d3bb9f4ed831ce
AlinaKapustina <alinakapystina@gmail.com>
2016-12-12 23:59:44 +0700

    добавлено скачивание

--- src/main/java/com/torrent/Client.java
+++ src/main/java/com/torrent/Client.java
@@ -1,15 +1,39 @@
 package com.torrent;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.RandomAccessFile;
+import java.util.List;
 
 public class Client {
-   
 
-    static void run(ArgsParser argsParser, TorrentParser torrentParser){
-       
-        QueueDownloader queueDownloader;
-        queueDownloader = new QueueDownloader(argsParser.getListIp(), torrentParser.getNumberPieces(),argsParser.getMyHost());
-       
-     
+    private static final String CLIENT = "./client/";
+
+    static void run(ArgsParser argsParser, TorrentParser torrentParser) {
+
+        QueueDownloader queueDownloader = queueDownloader = new QueueDownloader(argsParser.getListIp(), torrentParser.getNumberPieces(), argsParser.getMyHost());
+        String path = CLIENT + "client" + argsParser.getMyHost().getHostName() + "/";
+        Downloader downloader = new Downloader(queueDownloader, torrentParser.getPieces(), torrentParser.getPiecesLength(), torrentParser.getAllLength(), path);
+        downloader.run();
+        List<String> name = torrentParser.getName();
+        List<Integer> lenght = torrentParser.getLength();
+        int offset = 0;
+        try (RandomAccessFile in = new RandomAccessFile(path + "text.txt", "rw")) {
+            int number = torrentParser.getNumberFiles();
+            for (int i = 0; i < number; i++) {
+                try (OutputStream out = new FileOutputStream(path + name.get(i))) {
+                    int prom = lenght.get(i);
+                    byte[] buffer = new byte[prom];
+                    in.seek(offset);
+                    in.read(buffer, 0, prom);
+                    offset += prom;
+                    out.write(buffer);
+                }
+            }
+        } catch (IOException e) {
+        }
+        
     }
-    
+
 }
